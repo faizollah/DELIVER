17c795c9adbdf37b921178d254a5cea524a8b17d
Ali <faizollah2005@yahoo.com>
2025-08-11 15:28:59 +0100

    Build: move 'use client' to top of file for Next.js directive compliance

--- src/app/analysis/single/page.tsx
+++ src/app/analysis/single/page.tsx
@@ -1,5 +1,5 @@
-import Header from '@/components/Header';
 'use client';
+import Header from '@/components/Header';
 import { useState } from 'react';
 
 export default function SingleReviewPage() {
